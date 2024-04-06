5d649742b66829f4533941f8ecd8bc06eab5035f
Your Name <muhammadimronn98@gmail.com>
2024-04-06 22:36:16 +0500

    added parfumes page

--- src/pages/Aromates.jsx
+++ src/pages/Aromates.jsx
@@ -12,6 +12,7 @@ import Klavdia from '../images/Parfumes/Klavdia.jpg';
 import Woud from '../images/Parfumes/Woud.jpg';
 import Elixir from '../images/Parfumes/Elixir.jpg';
 import Jador from '../images/Parfumes/Jador.webp';
+import '../css/Aromates.css';
 
 export default function Aromates() {
   const aromates = [
@@ -102,7 +103,39 @@ export default function Aromates() {
   ];
   return (
     <div>
-      <Header />
+      <div className="aromatesMainDiv">
+        <Header />
+        <div className="aromatesMainDivTexts">
+          <p className="aromatesMainDivAroma">Ароматы</p>
+          <p className="aromatesMainDivP">Ваши клиенты будут в восторге:</p>
+          <p className="aromatesMainDivP">-Самый большой каталог ароматов - более 1500</p>
+          <p className="aromatesMainDivP">-Профессиональная помощи в подборе ароматов</p>
+          <p className="aromatesMainDivP">-Ароматы исключительно высшего качества!</p>
+        </div>
+      </div>
+      <div className="aromatesBody">
+        <p className="aromatesBodyHeader">Ваш бренд запомнят навсегда</p>
+        <p className="aromatesBodyP">
+          Создание хорошо различимых и притягательных ароматов — это искусство. В основе наших
+          ароматических масел лежат сложные специальные композиции, и мы используем только
+          высококачественные ингредиенты для создания ароматов. ScentAir может разработать аромат,
+          подходящий именно для Вашего бренда, — от обычных до нестандартных и даже индивидуальных
+          композиций. Ниже приведен лишь краткий список ароматов, предлагаемых компанией AromaAir.
+        </p>
+        <div className="aromates">
+          {aromates.map((item, index) => {
+            return (
+              <div className="aromatesCards">
+                <img className='cardImg' src={item.img} alt="" />
+                <h2 className='aromatesBodyH2'>{item.name}</h2>
+                <p className='aromatesBodyP2'>{item.a}</p>
+                <p className='aromatesBodyP2'>{item.b}</p>
+                <p className='aromatesBodyP2'>{item.c}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
